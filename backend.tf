824c33562cb1917d9f32c3a93f645ac985bd9363
peter.vanyo <peter.vanyo@emeo.sk>
2023-12-11 16:19:51 +0100

    chore: removed profile

--- backend.tf
+++ backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    profile = "<placeholder>"
     bucket  = "terraform-state-files-<placeholder>" #Please edit the name
     key     = "terraform.tfstate"
     region  = "eu-west-1"
